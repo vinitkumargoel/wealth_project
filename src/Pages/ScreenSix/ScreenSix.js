eb076a1cbe7a040ab340dbdf7db94bb7e770ae86
Vinit Kumar Goel <vinit928@gmail.com>
2019-08-06 16:44:49 +0530

    Six done

--- src/Pages/ScreenSix/ScreenSix.js
+++ src/Pages/ScreenSix/ScreenSix.js
@@ -2,12 +2,36 @@ import React, {
     Component
 } from 'react';
 
+import "./ScreenSix.css";
+
 class ScreenSix extends Component {
     state = {};
 
     render() {
         return (
-            <div>ScreenSix</div>
+            <div className="ScreenSix">
+                <h3>Appointment Booking</h3>
+                <p>
+                    According to your Postcode your local Adviser is Joe Bloggs at the Cheltenham branch. Not right? <a href="#">Click here.</a>
+                </p>
+                <p>
+                    You can talk to Joe face to face, on the phone or over Skype. Please select your preference below and the date and time that suits you.
+                </p>
+                <p>
+                    If you’re ready now please click on ‘talk to an adviser now’ and we’ll connect you to one of our dedicated video Advisers.
+                </p>
+
+                <div className="button_container">
+                    <button>Face to Face</button>
+                    <button>Phone</button>
+                    <button>Skype</button>
+                </div>
+                <div className="button_container">
+                    <button className="bottom_big_button">
+                        Talk to an adviser now
+                    </button>
+                </div>
+            </div>
         );
     }
 }
