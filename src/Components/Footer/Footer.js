327382585645ef48000080ed38348f5a33a15560
Vinit Kumar Goel <vinit928@gmail.com>
2019-08-06 15:56:29 +0530

    Footer done

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,13 +1,39 @@
 import React, {
     Component
 } from 'react';
+import "./Footer.css";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome, faLayerGroup, faHandshake, faQuestionCircle, faBars } from '@fortawesome/free-solid-svg-icons'
 
 class Footer extends Component {
     state = {};
-
     render() {
-        return (<div> Footer </div>
-        );
+        return (
+            <div>
+                <div className="style">
+                    <div className="selected_icon">
+                        <i><FontAwesomeIcon icon={faHome} /></i>
+                        <p>Home</p>
+                    </div>
+                    <div>
+                        <i><FontAwesomeIcon icon={faLayerGroup} /></i>
+                        <p>Products</p>
+                    </div>
+                    <div>
+                       <i> <FontAwesomeIcon icon={faHandshake} /></i>
+                        <p>Pay & Transfer</p>
+                    </div>
+                    <div>
+                        <i><FontAwesomeIcon icon={faQuestionCircle} /></i>
+                        <p>Support</p>
+                    </div>
+                    <div>
+                        <i><FontAwesomeIcon icon={faBars} /></i>
+                        <p>More</p>
+                    </div>
+                </div>
+            </div>);
     }
 }
 
