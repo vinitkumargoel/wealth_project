ba879fc2de321cee8fa42b6d2178d78e00daaa6c
sidharth jamuar <sidharth.jamuar1@wipro.com>
2019-08-06 18:30:24 +0530

    added arrows

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -3,7 +3,7 @@ import React, {
 } from 'react';
 import "./Card.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEllipsisV, faPoundSign } from '@fortawesome/free-solid-svg-icons'
+import { faEllipsisV, faPoundSign,faAngleRight } from '@fortawesome/free-solid-svg-icons'
 import LBGicon from "../../assets/LBG-icon.png"
 class Card extends Component {
     state = {};
@@ -15,7 +15,7 @@ class Card extends Component {
                 <div className="data-container">
                     <img src={LBGicon} width="40px" height="30px" />
                     <div className="details-container">
-                    {path==="/ScreenOne"?<span className="details-item-name">Club Platinum</span>:
+                    {path==="/ScreenOne"?<span className="details-item-name">{this.props.number===1?"Club Platinum":"Club Lloyds Saver Monthly"}</span>:
                     path==="/ScreenNine" && this.props.number===1?<span className="details-item-name">Club Platinum</span>:<span className="details-item-name">Scroders Personal Wealth</span>
         
                 }
@@ -26,10 +26,10 @@ class Card extends Component {
                     <i className="dot-icon-main"><FontAwesomeIcon icon={faEllipsisV} className="dot-icon" /></i>
                 </div>
                 {this.props.number===1 || path==="/ScreenOne"?<div className="money-container">
-                    <div><span className="pound-icon">£648.36</span></div>
+                    <div><span className="pound-icon">{this.props.number===1?"£648.36":"£12.18"}</span></div>
                     <div><span className="pound-icon">Available: </span><span className="pound-icon">£1023.76</span></div>
                 </div>:null}
-
+                {this.props.number===2 && path==="/ScreenNine"?<i><FontAwesomeIcon icon={faAngleRight} className="angle-right-content" /></i>:null}
             </div>
         );
     }
