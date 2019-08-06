e118502c9fa3e15a96f55cd1ec537b87349f3864
Vinit Kumar Goel <vinit928@gmail.com>
2019-08-06 16:15:07 +0530

    ScreenThree Done

--- src/Pages/ScreenThree/ScreenThree.js
+++ src/Pages/ScreenThree/ScreenThree.js
@@ -1,13 +1,26 @@
 import React, {
     Component
 } from 'react';
+import "./ScreenThree.css";
 
 class ScreenThree extends Component {
     state = {};
 
     render() {
         return (
-            <div>ScreenThree</div>
+            <div className="ScreenThree">
+                <h3>Understand Your Risk Appetite</h3>
+                <p>Would you be confortable investing for at least 5 Years?</p>
+
+                <button>Yes, Confortable</button>
+                <button>Perhaps for some of my money</button>
+                <button>No, not Confortable</button>
+
+                <p>Are you confortable with the liklihood that your money will raise and fall in value?</p>
+                <button>Yes, Confortable</button>
+                <button>Perhaps for some of my money</button>
+                <button>No, not Confortable</button>
+            </div>
         );
     }
 }
