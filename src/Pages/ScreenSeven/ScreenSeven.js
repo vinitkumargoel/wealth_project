cd372491cf93da9ca9a1a1b143ee2a19408e27a4
Vinit Kumar Goel <vinit928@gmail.com>
2019-08-06 16:52:48 +0530

    ScreenSeven Done

--- src/Pages/ScreenSeven/ScreenSeven.js
+++ src/Pages/ScreenSeven/ScreenSeven.js
@@ -2,11 +2,25 @@ import React, {
     Component
 } from 'react';
 
+import Calender from "../../assets/calender.png";
+
+import "./ScreenSeven.css";
+
 class ScreenSeven extends Component {
     state = {};
 
     render() {
-        return (<div> ScreenSeven </div>
+        return (
+            <div className="ScreenSeven">
+                <h3>Face to Face Appointment</h3>
+                <p>
+                    Thank you for selecting to see Joe Face to Face. You can see Joe’s calendar below. Please select a date and time that suits you.
+                </p>
+                <img src={Calender} />
+                <center>
+                    <button>Confirm</button>
+                </center>
+            </div>
         );
     }
 }
